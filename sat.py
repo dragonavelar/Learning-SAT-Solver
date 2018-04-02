cc3941277131a0f1c6de515eee9d93461dc33fb3
dragonavelar <dragonavelar@gmail.com>
2018-04-02 17:43:16 -0300

    Changed parameters of the run to be closer to the ones in the paper and added epoch and test summaries.

--- sat.py
+++ sat.py
@@ -40,7 +40,7 @@ def run_and_log_batch( epoch, b, batch, time_steps, train = True ):
 		#end if
 		# Print train step loss and accuracy, as well as predicted sat values compared with the normal ones
 		print(
-			"{timestamp}\t{memory}\tEpoch {epoch} Batch {batch} (n,m) ({n},{m}) Loss: {loss} Accuracy: {accuracy} PRED/REAL:".format(
+			"{timestamp}\t{memory}\tEpoch {epoch} Batch {batch} (n,m) ({n},{m}) Loss: {loss} Accuracy: {accuracy}".format(
 				timestamp = timestamp(),
 				memory = memory_usage(),
 				epoch = epoch,
@@ -50,16 +50,9 @@ def run_and_log_batch( epoch, b, batch, time_steps, train = True ):
 				n = batch.total_n,
 				m = batch.total_m
 			),
-			end = ""
+			flush = True
 		)
-		#for pred, real in list( zip( pred_SAT, sat ) ):
-		#	if abs( pred - real ) < 0.5:
-		#		print( " [{pred:.2f}, {real:.2f}]".format( pred = pred, real = real ), end = "" )
-		#	else:
-		#		print( " ({pred:.2f}, {real:.2f})".format( pred = pred, real = real ), end = "" )
-		#	#end if
-		##end for
-		print(flush = True)
+		return loss_val, accuracy_val
 #end run_and_log_batch
 
 if __name__ == "__main__":	
@@ -68,10 +61,9 @@ if __name__ == "__main__":
 	
 	time_steps = 26
 	batch_size = 64
-	batches_per_epoch = 50
-	# Since test instances are bigger
-	test_time_steps = 64
-	test_batch_size = 8
+	batches_per_epoch = 128
+	test_time_steps = time_steps
+	test_batch_size = batch_size
 	
 	# Build model
 	print( "{timestamp}\t{memory}\tBuilding model ...".format( timestamp = timestamp(), memory = memory_usage() ) )
@@ -91,16 +83,48 @@ if __name__ == "__main__":
 		# Run for a number of epochs
 		print( "{timestamp}\t{memory}\tRunning for {} epochs".format( epochs, timestamp = timestamp(), memory = memory_usage() ) )
 		for epoch in range( epochs ):
-			print( "{timestamp}\t{memory}\tTRAINING SET".format( timestamp = timestamp(), memory = memory_usage() ) )
+			# Reset training generator and run with a sample of the training instances
+			print( "{timestamp}\t{memory}\tTRAINING SET BEGIN".format( timestamp = timestamp(), memory = memory_usage() ) )
 			generator.reset()
+			epoch_loss = 0.0
+			epoch_accuracy = 0.0
 			for b, batch in itertools.islice( enumerate( generator.get_batches( batch_size ) ), batches_per_epoch ):
-				run_and_log_batch( epoch, b, batch, time_steps )
+				l, a = run_and_log_batch( epoch, b, batch, time_steps )
+				epoch_loss += l
+				epoch_accuracy += a
 			#end for
-			print( "{timestamp}\t{memory}\tTEST SET".format( timestamp = timestamp(), memory = memory_usage() ) )
+			epoch_loss = epoch_loss / batches_per_epoch
+			epoch_accuracy = epoch_accuracy / batches_per_epoch
+			print( "{timestamp}\t{memory}\tTRAINING SET END Mean loss: {loss} Mean Accuracy = {accuracy}".format(
+				loss = epoch_loss,
+				accuracy = epoch_accuracy,
+				timestamp = timestamp(),
+				memory = memory_usage()
+				)
+			)
+			# Summarize results and print epoch summary
+			test_loss = 0.0
+			test_accuracy = 0.0
+			test_batches = 0
+			# Reset test generator and run with the test instances
+			print( "{timestamp}\t{memory}\tTEST SET BEGIN".format( timestamp = timestamp(), memory = memory_usage() ) )
 			test_generator.reset()
 			for b, batch in enumerate( test_generator.get_batches( test_batch_size ) ):
-				run_and_log_batch( epoch, b, batch, test_time_steps, train = False )
+				l, a = run_and_log_batch( epoch, b, batch, test_time_steps, train = False )
+				test_loss += l
+				test_accuracy += a
+				test_batches += 1
 			#end for
+			# Summarize results and print test summary
+			test_loss = test_loss / test_batches
+			test_accuracy = test_accuracy / test_batches
+			print( "{timestamp}\t{memory}\tTEST SET END Mean loss: {loss} Mean Accuracy = {accuracy}".format(
+				loss = test_loss,
+				accuracy = test_accuracy,
+				timestamp = timestamp(),
+				memory = memory_usage()
+				)
+			)
 		#end for
 	#end with
 pass
