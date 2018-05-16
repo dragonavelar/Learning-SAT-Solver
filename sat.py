d318dea34e128b17dd770a10c3e115d26e2a4b51
Marcelo Prates <marceloorp@gmail.com>
2018-05-16 17:57:18 -0300

    Fixed number of epochs to 100, since it converges much earlier

--- sat.py
+++ sat.py
@@ -56,7 +56,7 @@ def run_and_log_batch( epoch, b, batch, time_steps, train = True ):
 #end run_and_log_batch
 
 if __name__ == "__main__":	
-	epochs = 1000
+	epochs = 100
 	d = 128
 	
 	time_steps = 26
